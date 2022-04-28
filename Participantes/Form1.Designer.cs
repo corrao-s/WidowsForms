27defedc04576da2716f6fedda01534917117b5b
Mariano <Mariano@EVC156-1>
2022-04-27 21:03:32 -0300

    Nuevo, Guarda, Cancelar, Eliminar, Mostrar datos en la grilla

--- Participantes/Form1.Designer.cs
+++ Participantes/Form1.Designer.cs
@@ -28,34 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtPuesto = new System.Windows.Forms.TextBox();
+            this.lblPuesto = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.dateFecha = new System.Windows.Forms.DateTimePicker();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnGuardar
             // 
-            this.button1.Location = new System.Drawing.Point(657, 400);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(97, 28);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Aceptar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnGuardar.Location = new System.Drawing.Point(196, 410);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(97, 28);
+            this.btnGuardar.TabIndex = 0;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(358, 12);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(430, 368);
+            this.dgv.TabIndex = 1;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(38, 46);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(41, 65);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(200, 22);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(41, 122);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(200, 22);
+            this.txtApellido.TabIndex = 5;
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(38, 103);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(57, 16);
+            this.lblApellido.TabIndex = 4;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(41, 238);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(200, 22);
+            this.txtAltura.TabIndex = 7;
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(38, 219);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(41, 16);
+            this.lblAltura.TabIndex = 6;
+            this.lblAltura.Text = "Altura";
+            // 
+            // txtPuesto
+            // 
+            this.txtPuesto.Location = new System.Drawing.Point(41, 294);
+            this.txtPuesto.Name = "txtPuesto";
+            this.txtPuesto.Size = new System.Drawing.Size(200, 22);
+            this.txtPuesto.TabIndex = 9;
+            // 
+            // lblPuesto
+            // 
+            this.lblPuesto.AutoSize = true;
+            this.lblPuesto.Location = new System.Drawing.Point(38, 275);
+            this.lblPuesto.Name = "lblPuesto";
+            this.lblPuesto.Size = new System.Drawing.Size(49, 16);
+            this.lblPuesto.TabIndex = 8;
+            this.lblPuesto.Text = "Puesto";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(38, 163);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(45, 16);
+            this.lblFecha.TabIndex = 10;
+            this.lblFecha.Text = "Fecha";
+            // 
+            // dateFecha
+            // 
+            this.dateFecha.Location = new System.Drawing.Point(41, 182);
+            this.dateFecha.Name = "dateFecha";
+            this.dateFecha.Size = new System.Drawing.Size(200, 22);
+            this.dateFecha.TabIndex = 11;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(311, 410);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(97, 28);
+            this.btnCancelar.TabIndex = 12;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(41, 352);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(97, 28);
+            this.btnNuevo.TabIndex = 13;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(691, 386);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(97, 28);
+            this.btnEliminar.TabIndex = 14;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.dateFecha);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.txtPuesto);
+            this.Controls.Add(this.lblPuesto);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.btnGuardar);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtPuesto;
+        private System.Windows.Forms.Label lblPuesto;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.DateTimePicker dateFecha;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnEliminar;
     }
 }
 
--- Participantes/Form1.cs
+++ Participantes/Form1.cs
@@ -12,14 +12,96 @@ namespace Participantes
 {
     public partial class Form1 : Form
     {
+        List<Participante> participantes = new List<Participante>();
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void LimpiarFormulario()
         {
-            MessageBox.Show("Hola");
+            txtNombre.Text = string.Empty;
+            txtApellido.Text = string.Empty;
+            txtAltura.Text = string.Empty;
+            txtPuesto.Text = string.Empty;
+            dateFecha.Text = string.Empty;
+        }
+        private void Bloquear(bool Editando)
+        {
+            btnNuevo.Enabled = !Editando;
+            btnGuardar.Enabled = Editando;
+            btnEliminar.Enabled = !Editando;
+            btnCancelar.Enabled = Editando;
+        }
+        private List<string> ValidarDatos()
+        {
+            List<string> errores = new List<string>();
+            if (string.IsNullOrEmpty(txtNombre.Text))
+            {
+                errores.Add("Debe ingresar el Nombre");
+            }
+            if (string.IsNullOrEmpty(txtApellido.Text))
+            {
+                errores.Add("Debe ingresar el apellido");
+            }
+            int a;
+            if (!int.TryParse(txtAltura.Text, out a))
+            {
+                errores.Add("La altura no es valida");
+            }
+            if (!int.TryParse(txtPuesto.Text, out a))
+            {
+                errores.Add("El puesto no es valido");
+            }
+            if (dateFecha.Value > DateTime.Now.AddYears(-18))
+            {
+                errores.Add("Debe ser mayor de 18 años");
+            }
+            return errores;
+        }
+        private void ActualizarGrilla()
+        {
+            dgv.DataSource = null;
+            dgv.DataSource = participantes;
+        }
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            LimpiarFormulario();
+            Bloquear(true);
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            List<string> errores = ValidarDatos();
+            if (errores.Count != 0)
+            {
+                MessageBox.Show(errores.Aggregate("", (acc, it) => acc + it + "\r\n").Trim());
+                return;
+            }
+            Participante p = new Participante(0, txtNombre.Text, txtApellido.Text, dateFecha.Value, int.Parse(txtAltura.Text), int.Parse(txtPuesto.Text));
+            participantes.Add(p);
+            ActualizarGrilla();
+            Bloquear(false);
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimpiarFormulario();
+            Bloquear(false);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgv.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("Debe seleccionar un elemento de la lista para eliminar");
+            }
+            else if (MessageBox.Show("Esta Seguro?","Eliminar",MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                participantes.RemoveAt(dgv.SelectedRows[0].Index);
+                ActualizarGrilla();
+            }
         }
     }
 }
